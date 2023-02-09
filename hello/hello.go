e22ce433556254d1aaf6e6174785cc86fa9d7301
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-02-09 21:00:29 +0200

    Module import

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Opt())
+	// Get a greeting message and print it
+	message := greetings.Hello("Myroslav")
+
+	fmt.Println(message)
 }
